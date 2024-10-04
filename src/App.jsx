6ce93e67f63ebb148a1772a8c4f82c3905b61674
Likhon Sheikh <likhondocs@gmail.com>
2024-10-04 11:02:10 +0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import VideoList from './components/VideoList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Video Scraper</h1>
+      <VideoList />
+    </div>
+  );
+}
+
+export default App;
